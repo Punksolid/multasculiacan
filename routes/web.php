77e17d8adcb8a81532410bd742477469ff2ab109
Jose Palazuelos <punksolid@gmail.com>
2018-06-23 01:11:09 -0600

    Revert "gitflow-feature-stash: to_delete"
    This reverts commit 1934ddd68835294320be95ab58ecf89590c537cc.

--- routes/web.php
+++ routes/web.php
@@ -42,14 +42,3 @@ Route::any('extraer_vehiculo/{placa}', 'MultasController@extraer_vehiculo');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
-
-Route::get('/jp', function (){
-    $frases = [
-        "fiiiinta!",
-        "así nomás quedó"
-
-    ];
-    $element = array_random($frases);
-
-   return view("jp", ["frase" => $element]);
-});
